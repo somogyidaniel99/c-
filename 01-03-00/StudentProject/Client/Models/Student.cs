885efc5e6e2808ec84653dfdfc5d15c3b8d6c6d4
Somogyi Dániel <115460013+somogyidaniel99@users.noreply.github.com>
2024-01-16 00:15:37 +0100

    Add files via upload

---
+++ 01-03-00/StudentProject/Client/Models/Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace StudentProject.Models
+{
+    public enum SchoolClassType { ClassA, ClassB, ClassC}
+
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthsDay { get; set; }
+        public int SchoolYear { get; set; }
+        public SchoolClassType SchoolClass { get; set; }
+        public string EducationLevel { get; set; }
+
+        public Student(string firstName, string lastName, DateTime birthsDay, int schoolYear, SchoolClassType schoolClass, string educationLevel)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthsDay = birthsDay;
+            SchoolYear = schoolYear;
+            SchoolClass = schoolClass;
+            EducationLevel = educationLevel;
+        }
+
+        public Student()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            BirthsDay = new DateTime();
+            SchoolYear = 9;
+            SchoolClass = SchoolClassType.ClassA;
+            EducationLevel = string.Empty;
+        }
+
+        public string HungarianFullName => $"{LastName} {FirstName}";
+        public string HungarianBirthsDay => BirthsDay.ToString("yyyy.MM.dd.");
+        public string HungarianLongBirthsDay => BirthsDay.ToString("yyyy.MM.dd. dddd");
+
+        public string SchoolYearAndClass
+        {
+            get
+            {
+                string schoolClassName = string.Empty;
+                switch (SchoolClass)
+                {
+                    case SchoolClassType.ClassA: schoolClassName = "a"; break;
+                    case SchoolClassType.ClassB: schoolClassName = "b"; break;
+                    case SchoolClassType.ClassC: schoolClassName = "c"; break;
+                }
+                if (schoolClassName != string.Empty)
+                    return $"{SchoolYear}.{schoolClassName}";
+                else
+                    return $"{SchoolYear} évfolyam";
+            }
+        }
+
+        public string AllProperty => $"{LastName} {FirstName} ({SchoolYearAndClass}) \n{HungarianBirthsDay}\n{EducationLevel}";
+
+        public override string ToString()
+        {
+            return AllProperty;
+        }
+    }
+}
---
+++ 01-03-00/StudentProject/Client/ViewModels/StudentViewModel.cs
@@ -0,0 +1,61 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using StudentProject.Models;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace StudentProject.ViewModels
+{
+    public partial class StudentViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private ObservableCollection<string> _educationLevels = new ObservableCollection<string>(new EducationLevels().AllEducationLevels);
+
+        [ObservableProperty]
+        private ObservableCollection<Student> _students = new ObservableCollection<Student>();
+
+        [ObservableProperty]
+        private Student _selectedStudent;
+
+        private string _selectedEducationLevel=string.Empty;
+        public string SelectedEducationLevel
+        {
+            get => _selectedEducationLevel;
+            set
+            {
+                SetProperty(ref _selectedEducationLevel, value);
+                SelectedStudent.EducationLevel = _selectedEducationLevel;
+            }
+        }    
+
+        public StudentViewModel()
+        {
+            Students.Add(new Student("Elek", "Teszt", System.DateTime.Now, 9, SchoolClassType.ClassA, "érettségi"));
+            Students.Add(new Student("János", "Teszt", System.DateTime.Now, 10, SchoolClassType.ClassB, "érettségi"));
+            Students.Add(new Student("Teréz", "Teszt", System.DateTime.Now, 11, SchoolClassType.ClassC, "érettségi"));
+
+            SelectedStudent = new Student();
+            SelectedEducationLevel=_educationLevels.ElementAt(0);
+        }
+
+        [RelayCommand]
+        public void DoSave(Student newStudent)
+        {
+            Students.Add(newStudent);
+            OnPropertyChanged(nameof(Students));
+        }
+
+        [RelayCommand]
+        void DoNewStudent()
+        {
+            SelectedStudent = new Student();
+        }
+
+        [RelayCommand]
+        public void DoRemove(Student studentToDelete)
+        {
+            Students.Remove(studentToDelete);
+            OnPropertyChanged(nameof(Students));
+        }
+    }
+}
